e457357a10fd017adc754e4f2d5c2c9462b6eb1a
Ervin <ervinnvs65@gmail.com>
2022-10-19 07:20:24 -0500

    Primer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Navigation } from "./routes";
+
+import { ClientLayout } from "./layouts";
+
+export default function App() {
+  return (
+    <ClientLayout className="app">
+      <h1 className="app__title">Hola Mundo!</h1>
+      <Navigation />
+    </ClientLayout>
+  );
+}
---
+++ src/routes/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Routes } from "react-router-dom";
+
+export function Navigation() {
+  return (
+    <Routes>
+    
+      <h2>Navigation...</h2>
+    </Routes>
+  );
+}
